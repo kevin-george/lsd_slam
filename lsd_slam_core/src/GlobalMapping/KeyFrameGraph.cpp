3a07b53a5c74b2881119f7e2818fa1ea98144710
sven-hoek <herrlauch@gmail.com>
2019-10-25 15:08:21 +0300

    Fix package for ROS melodic under Ubuntu 18.04.

--- lsd_slam_core/src/GlobalMapping/KeyFrameGraph.cpp
+++ lsd_slam_core/src/GlobalMapping/KeyFrameGraph.cpp
@@ -80,9 +80,9 @@ KeyFrameGraph::KeyFrameGraph()
         typedef g2o::BlockSolver_7_3 BlockSolver;
         typedef g2o::LinearSolverCSparse<BlockSolver::PoseMatrixType> LinearSolver;
         //typedef g2o::LinearSolverPCG<BlockSolver::PoseMatrixType> LinearSolver;
-        LinearSolver* solver = new LinearSolver();
-        BlockSolver* blockSolver = new BlockSolver(solver);
-        g2o::OptimizationAlgorithmLevenberg* algorithm = new g2o::OptimizationAlgorithmLevenberg(blockSolver);
+        std::unique_ptr<LinearSolver> solver(new LinearSolver());
+        std::unique_ptr<BlockSolver> blockSolver(new BlockSolver(std::move(solver)));
+        g2o::OptimizationAlgorithmLevenberg* algorithm = new g2o::OptimizationAlgorithmLevenberg(std::move(blockSolver));
         graph.setAlgorithm(algorithm);
 
 	
